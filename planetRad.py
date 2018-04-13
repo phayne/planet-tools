a81597cd57d9ea3da62458adf3deb7fa18139cd8
Paul Hayne <31781956+phayne@users.noreply.github.com>
2018-04-13 16:58:19 -0600

    Add files via upload

---
+++ planetRad.py
@@ -0,0 +1,202 @@
+"""
+Radiation and radiative transfer tools for planetary science
+"""
+
+# Physical constants
+sigma = 5.670367e-8 # Stefan-Boltzmann constant [W.m-2.K-4]
+
+# Constants for wavelength-based Planck function
+c1 = 1.191043e-16 # 2*h*c**2
+c2 = 0.01438777   # h*c/k
+
+# Constants for frequency-based Planck function
+a1 = 1.191066e-5
+a2 = 1.438833
+
+# NumPy is needed for various math operations
+import numpy as np
+np.seterr(over='ignore') # suppress overflow warnings
+
+# Valid temperature range and increment
+TMIN =  1.0 # Minimum valid temperature [K]
+TMAX = 600.0 # Maximum valid temperature [K]
+DT = 1.0 # Temperature increment/precision [K]
+TRANGE = np.arange(TMIN, TMAX, DT) # Temperature array
+
+##########
+# planck #
+##########
+# Planck's function for blackbody radiance in SI-wavelength units:
+# Input:
+#    T = temperature [K]
+#    lam = wavelength [m]
+# Output:
+#    spectral radiance [W.m-2.sr-1.m-1]
+def planck(T, lam):
+    r = c1*lam**-5 / (np.exp(c2*(lam*T)**-1) - 1)
+    return r
+
+############
+# planckwn #
+############
+# Planck's function for blackbody radiance in frequency units:
+# Input:
+#    T = temperature [K]
+#    nu = frequency [cm-1]
+# Output:
+#    spectral radiance [mW.m-2.sr-1.cm]
+def planckwn(T, nu):
+    r = a1*nu**3 / (np.exp(a2*nu/T) - 1)
+    return r
+
+###########
+# iplanck #
+###########
+# Inverse of Planck's function in SI-units:
+# Input:
+#    R = radiance [W.m-2.sr-1.m-1]
+#    lam = wavelength [m]
+# Output:
+#    brightness temperature [K]
+def iplanck(R, lam):
+    Tb = c2 / ( lam * np.log( (c1/(lam**5*R)) + 1 ) )
+    return Tb
+
+########################
+# effective_wavelength #
+########################
+# Radiance- and response-weighted wavelength for a
+# given spectral response function and scene temperature
+# Input:
+#    f = spectral response function
+#    lam = wavelength in METERS
+#    T = scene temperature in Kelvin
+# Output:
+#    Effective wavelength in meters
+def effective_wavelength(f, lam, T):
+    b = planck(T,lam)
+    lam_eff = np.trapz(lam*f*b,lam)/np.trapz(f*b,lam)
+    
+    return lam_eff
+########################
+
+##################
+# radiance_to_tb #
+##################
+# Convert a measured radiance to brightness
+# temperature (equivalent blackbody temperature), 
+# for the given spectral response function
+# Input:
+#    f = spectral response function
+#    lam = wavelength in METERS
+#    radiance = radiance in SI units: W.m-2.sr-1.m-1
+# Output:
+#    brightness temperature [K]
+def radiance_to_tb(f, lam, radiance):
+    b = tb_to_radiance(f, lam, TRANGE)
+    tb = np.interp(radiance, b, TRANGE)
+    
+    return tb
+
+##################
+# tb_to_radiance #
+##################
+# Convert a brightness temperature (equivalent 
+# blackbody temperature) to radiance, for the 
+# given spectral response function
+# Input:
+#    f = spectral response function
+#    lam = wavelength in METERS
+#    tb = brightness temperature [K]
+# Output:
+#    radiance [W.m-2.sr-1.m-1]
+def tb_to_radiance(f, lam, tb):
+    # Check length of tb
+    if hasattr(tb, "__len__"):
+        radiance = []
+        for T in tb:
+            b = planck(T, lam)
+            radiance.append( np.trapz(f*b,lam) )
+    else:
+        b = planck(tb, lam)
+        radiance = np.trapz(f*b,lam)
+     
+    return radiance
+
+###########################
+# equilibrium_temperature #
+###########################
+# Equilibrium temperature for rapid rotator
+# Assumes constant surface temperature, i.e. effectively
+# infinite thermal inertia over the thermal skin depth
+# Input:
+#    F = incident solar flux [W.m-2]
+#    A = bolometric solar albedo (i.e., Bond albedo)
+#    e = thermal bolometric emissivity
+# Output:
+#    T = temperature [K]
+def equilibrium_temperature(F, A, e):
+    return ((1-A)*F/(4*e*sigma))**0.25
+
+##########################
+# albedo_layer_twostream #
+##########################
+# Calculate albedo** for a layer of finite optical
+# thickness and known optical properties, overlying
+# a layer of known albedo. This model uses the
+# well-established delta-Eddington two-stream method to
+# approximate radiative transfer in the two-layer medium.
+#
+# **The quantity calculated is the "directional-hemispheric
+# reflectance" or "hemispheric albedo" (see: Hapke, 1981,
+# J. Geophys. Res., v. 86, p. 3049). The approach taken
+# here is that of Wiscombe and Warren (1980), J. Atmos.
+# Sci., v. 37, pp. 2712-2733. The layer is assumed to
+# be composed of discrete particles (grains), whose
+# optical properties can be approximated using Mie
+# theory.
+# 
+# Input:
+#    tau = optical thickness of the layer [scalar or array]
+#    w0 = single-scattering albedo of grains [scalar]
+#    g = asymmetry parameter, g = <cos(phase)> [scalar]
+#    mu = cosine of the emission angle [scalar]
+#    asurf = albedo of underlying surface
+# Output:
+#    directional-hemispheric reflectance
+def albedo_layer_twostream(tau, w0, g, mu, asurf):
+    
+    # Cutoff criterion for semi-infinite approx.
+    CUTOFF_H = 1e3
+    CUTOFF_L = 1e-3
+    
+    # DEBUG
+    #print(w0p/(1+p) * (1-bp*x*mu)/(1+x*mu))
+    
+    # Transform to delta-Eddington coordinates
+    g2 = g**2
+    taup = (1-w0*g2)*tau
+    w0p = (1-g2)*w0/(1-g2*w0)
+    gp = g/(1+g)
+    
+    # Define some useful parameters
+    ap = 1 - w0p*gp
+    bp = gp/ap
+    x = np.sqrt(3*ap*(1-w0p))
+    p = (2/3)*x/ap
+    gamma = (1-asurf)/(1+asurf)
+    qp = (gamma+p)*np.exp(+x*taup)
+    qm = (gamma-p)*np.exp(-x*taup)
+    q = (1+p)*qp - (1-p)*qm
+    
+    # Check whether the layer is optically thick;
+    # if so, use a simplified formula
+    if ( qp>CUTOFF_H or qm<CUTOFF_L ) :
+        albedo = w0p/(1+p) * (1-bp*x*mu)/(1+x*mu)
+    # Otherwise, use the full formula
+    else:
+        albedo = (1/q) * ( 2*( p*(1-gamma + w0p*bp) + w0p*(1+bp)*(gamma*x*mu - p)/(1 - (x*mu)**2))  \
+                             * np.exp(-taup/mu) - w0p*bp*(qp-qm) \
+                             + w0p*(1+bp)*(qp/(1+x*mu) - qm/(1-x*mu)) ) 
+    
+    return albedo
---
+++ planetThermo.py
@@ -0,0 +1,112 @@
+"""
+Thermodyanmic functions and constants for planetary science
+"""
+
+# Load Python dependencies
+import numpy as np
+
+######################
+# Physical constants #
+######################
+
+# Pi
+pi = np.pi
+
+# Proton mass
+mp = 1.6726e-27
+
+# Molecular masses [kg]
+m_h2o = 3.0132e-26
+
+# Molar masses [kg.mole-1]
+mw_h2o = 18.015e-3   # Water
+
+# Thermodynamic constants
+R = 8.31447 # Universal gas constant [J.mol-1.K-1]
+k_B = 1.38064852e-23 # Boltzmann constant [m2.kg.s-2.K-1]
+
+class volatile:
+    '''
+    A volatile is a molecular species that undergoes
+    phase transitions at planetary surface and/or atmospheric
+    temperatures
+    '''
+    #__repr__ object prints out a help string when help is
+    #invoked on the volatile object or the volatile name is typed
+    def __repr__(self):
+        line1 =\
+        'This volatile object contains information on %s\n'%self.name
+        line2 = 'Type \"help(volatile)\" for more information\n'
+        return line1+line2
+    def __init__(self):
+        self.name = None #Name of the volatile
+
+########
+# ph2o #
+########
+# -----------------------------------------------
+# Equilibrium vapor pressure over water over ice
+# Marti and Mauersberger (1993)
+# -----------------------------------------------
+# Input:
+#    T = temperature of ice [K]
+# Output:
+#    vapor pressure [Pa]
+def ph2o(T):
+    A = -2663.5
+    B = 12.537
+    p = 10**(A/T + B)
+
+    return p
+
+########
+# pco2 #
+########
+# --------------------------------------------
+# Equilibrium vapor pressure over solid CO2
+# Brown and Ziegler (1980)
+# --------------------------------------------
+# Input:
+#    T = temperature of solid [K]
+# Output:
+#    vapor pressure [Pa]
+def pco2(T):
+    A0 = 2.13807649*10**1
+    A1 = -2.57064700*10**3
+    A2 = -7.78129489*10**4
+    A3 = 4.32506256*10**6
+    A4 = -1.20671368*10**8
+    A5 = 1.34966306*10**9
+
+    # Pressure in torr
+    ptorr = np.exp(A0 + A1/T + A2/T**2 + A3/T**3 + A4/T**4 + A5/T**5)
+
+    # Pressure in Pa
+    p = 133.3223684211*ptorr
+
+    return p
+
+
+######################
+# h2oSublimationRate #
+######################
+# -------------------------------------------------------------------------
+# Water ice sublimation rate, in kg.m-2.s-1, based on planar surface
+# solution from Estermann (1955): mdot = pv*sqrt(mw_h2o/2piRT), where mh2o
+# is the molecular weight of water, R is the universal gas constant, T is
+# temperature in Kelvin, and pv is the saturation vapor pressure. This is
+# the upper limit on sublimation rate, where the actual sublimation rate
+# depends on the difference (p-pv) and the rate of adsorption.
+# -------------------------------------------------------------------------
+# Input:
+#    T = temperature of ice [K]
+# Output:
+#    sublimation rate [kg.m-2.s-1]
+def h2oSublimationRate(T):
+    # Equilibrium water vapor pressure over ice
+    pv = ph2o(T) # [Pa]
+
+    # Sublimation rate
+    mdot = pv * np.sqrt( mw_h2o/(2*pi*R*T) ) # [kg.m-2.s-1]
+
+    return mdot
